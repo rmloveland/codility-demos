42732c32fff99e4354b997154505b5baa4e51410
Rich Loveland <loveland.richard@gmail.com>
2012-02-07 22:09:30 -0500

    First commit, a rough working version.

---
+++ equi.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'test/unit'
+
+# See a description of the problem at
+# http://blog.codility.com/2011/03/solutions-for-task-equi.html
+
+def equi(a)
+  ## Array -> Int
+  index = a.length-1
+  results = []
+  while index > 0
+    first, rest = a[0..index-1], a[index+1..a.length-1]
+    sum_first, sum_rest = first.reduce(:+), rest.reduce(:+)
+    if sum_first == sum_rest
+      results.push index
+    elsif first.nil? or rest.nil?
+      results.push index
+    end
+    index -= 1
+  end
+  results[0]
+end
+
+class TestEqui < Test::Unit::TestCase
+  def test_equi
+    sample_array = [-7, 1, 5, 2, -4, 3, 0]
+    expected = equi(sample_array)
+    assert_equal expected, 3
+  end
+end
